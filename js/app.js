b660cfc081de380164a050f4e50d74bbccf4e767
NicoSimplon <marty.nicolas11@laposte.net>
2018-02-28 16:51:26 +0100

    version fonctionnelle mais pas en ajax

--- js/app.js
+++ js/app.js
@@ -2,102 +2,44 @@
 moment.locale("fr");
 $("#date").text(moment().format('LL'));
 
-//afficher les données météo
+//afficher les données météo et la carte (au chargement ce sont les données de Pamiers qui sont chargées)
 var ville;
-var pamiers = "http://api.openweathermap.org/data/2.5/weather?q=Pamiers&units=metric&lang=fr&appid=1ad29eaefcd945e4f49ae28974ac9165"
 
-
-fetch(pamiers)
+fetch("http://api.openweathermap.org/data/2.5/weather?q=Pamiers&units=metric&lang=fr&appid=1ad29eaefcd945e4f49ae28974ac9165")
 	.then(res => res.json())
-	.then(resJson => $("#temp").text(parseInt(resJson.main.temp) + '°'))
+	.then(resJson => {
+						$("#temp").text(parseInt(resJson.main.temp) + '°'), 
+						$("#tempMax").text(resJson.main.temp_max),
+						$("#tempMin").text(resJson.main.temp_min),
+						$("#presAthmos").text(resJson.main.pressure + ' Pa'),
+						$("#humidite").text(resJson.main.humidity),
+						$("#vitesseVent").text(resJson.wind.speed + ' km/h'),
+						$("#longitude").text(resJson.coord.lon),
+						$("#latitude").text(resJson.coord.lat),
+						$("#carte").html("<iframe  src='https://www.google.com/maps/embed/v1/place?key=AIzaSyBMtdsVDGb8x9NJku3jdI3eFfAL9tu22ao&q=Pamiers&zoom=12&maptype=roadmap' width='100%' height='100%' frameborder='0'></iframe>")
+					})
 
-fetch(pamiers)
-	.then(res => res.json())
-	.then(resJson => $("#tempMax").text(resJson.main.temp_max))
-
-fetch(pamiers)
-	.then(res => res.json())
-	.then(resJson => $("#tempMin").text(resJson.main.temp_min))
-
-fetch(pamiers)
-	.then(res => res.json())
-	.then(resJson => $("#presAthmos").text(resJson.main.pressure + ' Pa'))
-
-fetch(pamiers)
-	.then(res => res.json())
-	.then(resJson => $("#humidite").text(resJson.main.humidity))
-
-fetch(pamiers)
-	.then(res => res.json())
-	.then(resJson => $("#vitesseVent").text(resJson.wind.speed + " km/h"))
-
-fetch(pamiers)
-	.then(res => res.json())
-	.then(resJson => $("#longitude").text(resJson.coord.lon))
-
-fetch(pamiers)
-	.then(res => res.json())
-	.then(resJson => $("#latitude").text(resJson.coord.lat))
-
-$("#submit").click(function(){
-	ville = $("#address").val();
-	var fech = "http://api.openweathermap.org/data/2.5/weather?q="+ ville +"&units=metric&lang=fr&appid=1ad29eaefcd945e4f49ae28974ac9165"
-
-	fetch(fech)
-		.then(res => res.json())
-		.then(resJson => $("#temp").text(parseInt(resJson.main.temp) + '°'))
-
-	fetch(fech)
-		.then(res => res.json())
-		.then(resJson => $("#tempMax").text(resJson.main.temp_max))
-
-	fetch(fech)
-		.then(res => res.json())
-		.then(resJson => $("#tempMin").text(resJson.main.temp_min))
-
-	fetch(fech)
-		.then(res => res.json())
-		.then(resJson => $("#presAthmos").text(resJson.main.pressure + ' Pa'))
-
-	fetch(fech)
-		.then(res => res.json())
-		.then(resJson => $("#humidite").text(resJson.main.humidity))
-
-	fetch(fech)
-		.then(res => res.json())
-		.then(resJson => $("#longitude").text(resJson.coord.lon))
-
-	fetch(fech)
-		.then(res => res.json())
-		.then(resJson => $("#latitude").text(resJson.coord.lat))
+$("#ville").keypress(function(event){
+  if ( event.which == 13 ) {
+     event.preventDefault();
+  }
 });
 
-// afficher la carte
+$("#bouton").click(function(){
 
-function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
-        center: {lat: 43.11, lng: 1.61}
-    });
-    var geocoder = new google.maps.Geocoder();
+	ville = $("#ville").val();
 
-    document.getElementById('submit').addEventListener('click', function() {
-        geocodeAddress(geocoder, map);
-    });
-}
-
-function geocodeAddress(geocoder, resultsMap) {
-    var address = document.getElementById('address').value;
-    geocoder.geocode({'address': address}, function(results, status) {
-        if (status === 'OK') {
-            resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-            	map: resultsMap,
-            	position: results[0].geometry.location
-            });
-        } 
-        else {
-            alert('Geocode was not successful for the following reason: ' + status);
-        }
-    });
-}
+	fetch("http://api.openweathermap.org/data/2.5/weather?q="+ ville +"&units=metric&lang=fr&appid=1ad29eaefcd945e4f49ae28974ac9165")
+		.then(res => res.json())
+		.then(resJson => {
+							$("#temp").text(parseInt(resJson.main.temp) + '°'), 
+							$("#tempMax").text(resJson.main.temp_max),
+							$("#tempMin").text(resJson.main.temp_min),
+							$("#presAthmos").text(resJson.main.pressure + ' Pa'),
+							$("#humidite").text(resJson.main.humidity),
+							$("#vitesseVent").text(resJson.wind.speed + ' km/h'),
+							$("#longitude").text(resJson.coord.lon),
+							$("#latitude").text(resJson.coord.lat),
+							$("#carte").html("<iframe  src='https://www.google.com/maps/embed/v1/place?key=AIzaSyBMtdsVDGb8x9NJku3jdI3eFfAL9tu22ao&q="+ville+"&zoom=12&maptype=roadmap' width='100%' height='100%' frameborder='0'></iframe>")
+						})
+});
